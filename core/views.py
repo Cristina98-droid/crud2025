004acb8aefe95983b52c36a8605c7a40c6fd5d00
cris071798 <mc.osorio@ebc.edu.mx>
2025-02-10 20:28:19 -0600

    prueba

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render ,HttpResponse
+
+# Create your views here.
+
+def home (request):
+    html_response = "<h1> Accesorios Sofi </h1>"
+    for i in range(10):
+        html_response += "<p> Esto es la portada </p>"
+    return HttpResponse (html_response)
